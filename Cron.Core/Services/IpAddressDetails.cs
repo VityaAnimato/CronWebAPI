e84049fa64d1a0aa475b1f5edc09c93c37a24b6b
VityaAnimato <vityaanimato@gmail.com>
2023-12-13 17:02:15 +0200

    init

---
+++ Cron.Core/Services/IpAddressDetails.cs
@@ -0,0 +1,20 @@
+﻿using static System.Net.WebRequestMethods;
+
+namespace Cron.Core.Services
+{
+    public class IpAddressDetails
+    {
+        public static async Task<string> Get(string requestedIP)
+        {
+            using (HttpClient httpclient = new HttpClient())
+            {
+                requestedIP = "https://ipinfo.io/" + requestedIP + "/geo";
+                HttpResponseMessage responce = await httpclient.GetAsync(requestedIP);
+                string json = await responce.Content.ReadAsStringAsync();
+                return json;
+            }
+        }
+    }
+
+}
+
